654674e30916ab07fd4027f61a8a6b52fdc49abf
Justin Donaldson <jdonaldson@gmail.com>
2017-11-10 12:14:29 -0800

    add a cascading merge

--- src/tink/Anon.hx
+++ src/tink/Anon.hx
@@ -14,13 +14,25 @@ class Anon {
     var ct = type.toComplex();
     return
       mergeExpressions(
-        exprs, 
+        exprs,
         requiredFields(type),
         ct
       );
   }
 
-  macro static public function splat(e:Expr, ?prefix:Expr, ?filter:Expr) 
+  public static macro function cascade<T>(exprs : Array<ExprOf<T>>) : Expr {
+    var type = Context.getExpectedType();
+    var ct = type.toComplex();
+    return
+      mergeExpressions(
+        exprs,
+        requiredFields(type),
+        ct,
+        true
+      );
+  }
+
+  macro static public function splat(e:Expr, ?prefix:Expr, ?filter:Expr)
     return makeSplat(e, prefix, filter);
 
 }

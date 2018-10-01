c05428d4f03609907864e55fb82050d513d70806
Juraj Kirchheim <back2dos@googlemail.com>
2018-10-01 14:05:51 +0200

    Add helper to grab existent fields in typed manner.

--- src/tink/Anon.hx
+++ src/tink/Anon.hx
@@ -9,6 +9,25 @@ using tink.MacroApi;
 #end
 
 class Anon {
+  
+  static function getExistentFields(o:{}):{} {
+    var ret = {};
+    for (f in Reflect.fields(o)) Reflect.setField(ret, f, true);
+    return ret;
+  }
+
+  macro static public function existentFields(e:Expr) {
+    var t = TAnonymous([
+      for (f in e.typeof().sure().getFields().sure()) 
+        if (f.isPublic && f.meta.has(':optional')) {
+          name: f.name,
+          kind: FProp('default', 'never', macro : Bool),
+          pos: f.pos,
+        } 
+    ]);
+    return macro @:pos(e.pos) @:privateAccess (cast tink.Anon.getExistentFields($e):$t);
+  }
+  
   macro static public function merge(exprs:Array<Expr>) {
     var type = Context.getExpectedType();
     var ct = type.toComplex();
